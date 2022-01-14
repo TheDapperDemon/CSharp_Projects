c6b5d5910c4b7cbe1e287704c3eb8047ef008f52
TheDapperDemon <83481628+TheDapperDemon@users.noreply.github.com>
2022-01-14 16:39:40 -0700

    Added Arrays file

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] stringArray = { "Your Name is: ", "Your Ageis: ", "Your Height is: ", "Your Favorite Color is: "};
+            Console.WriteLine("What is your Name?");
+            string answer = Console.ReadLine();
+            Console.WriteLine(stringArray [1], answer);
+            Console.ReadLine();
+        }
+    }
+}
