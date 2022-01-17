f030740df73387d04dcca8102adb1b9078a8de7b
TheDapperDemon <83481628+TheDapperDemon@users.noreply.github.com>
2022-01-17 16:58:19 -0700

    created and updated Pt6

---
+++ ConsoleAppPt6/ConsoleAppPt6/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleAppPt6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> colors = new List<string>() { "black", "purple", "orange", "blue", "brown", "green", "red", "blue" };
+            Console.WriteLine("enter a color to search list");
+            string answer = Console.ReadLine();
+            int Counter = 0;
+                foreach (string i in colors)
+                    {
+                    if (i == answer)
+                            
+                            {
+                            if (Counter == 0)
+                            { 
+                            Console.WriteLine(answer + " is on List!");
+                            Counter++;
+                            }
+                            else
+                            {
+                             Console.WriteLine(answer + " is on list but has already been found");
+                            }
+                    }
+                    else
+                    {
+                    Console.WriteLine(answer + " is not on List");
+                    }
+
+                }
+                 Console.ReadLine();
+        }
+    }
+}
