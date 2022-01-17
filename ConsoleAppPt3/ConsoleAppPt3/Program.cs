4333a21293e4a052d932fe7cf6e88505bcefc583
TheDapperDemon <83481628+TheDapperDemon@users.noreply.github.com>
2022-01-17 12:36:25 -0700

    ConsoleAppPt3

---
+++ ConsoleAppPt3/ConsoleAppPt3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleAppPt3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 20;
+            int B = 10;
+
+            while (A<=B)
+            {
+                Console.WriteLine("A wins!");
+                Console.ReadLine();
+                B++;
+            }
+          
+        }
+    }
+}
