ed4a784eecd236ae72afbb23e0cb09f9bd75fc19
TheDapperDemon <83481628+TheDapperDemon@users.noreply.github.com>
2022-01-17 16:34:59 -0700

    Created ConsoleAppPt5

---
+++ ConsoleAppPt5/ConsoleAppPt5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleAppPt5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> animals = new List<string>() { "cat", "dog", "bird", "cat" };
+            Console.WriteLine("enter an animal to search list");
+            string answer = Console.ReadLine();
+            for (int i = 0; i < animals.Count; i++)
+            {
+                if (animals[i] == answer)
+                {
+                    Console.WriteLine(answer + " is on List!");
+                }
+                else
+                {
+                    Console.WriteLine(answer + " is not on List");
+                }
+
+            }
+            Console.ReadLine();
+        }
+    }
+    
+}
