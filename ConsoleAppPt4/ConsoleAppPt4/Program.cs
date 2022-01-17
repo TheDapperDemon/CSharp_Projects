74032a696425b9e8f1b88cdd3fa67e3b5bd03754
TheDapperDemon <83481628+TheDapperDemon@users.noreply.github.com>
2022-01-17 15:06:37 -0700

    updsated pt 4

---
+++ ConsoleAppPt4/ConsoleAppPt4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleAppPt4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> colors = new List<string>() { "black", "purple", "orange", "blue", "brown", "green", "red" };
+            Console.WriteLine("");
+            string answer = Console.ReadLine();
+            for (int i = 0; i < stringArray.Length;)
+            for (int e = 1; e < stringArray.Length;)
+            for (int o = 2; o < stringArray.Length;)
+            for (int u = 3; u < stringArray.Length;)
+                        {
+                stringArray[i] += answer;
+                Console.WriteLine(stringArray[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
