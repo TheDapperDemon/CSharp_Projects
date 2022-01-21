f7cbb3511001991f9bcafa0469d2a22a16334c3a
TheDapperDemon <83481628+TheDapperDemon@users.noreply.github.com>
2022-01-21 14:53:29 -0700

    Created Pt7

---
+++ ConsoleAppPt7/ConsoleAppPt7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleAppPt7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> Integers = new List<string>() { "35", "15", "22", "91" };
+            Console.WriteLine("Please enter a number to be divide by each number in the list");
+            var answer = Console.ReadLine();
+                for (int i = 0; i < Integers.Count; i++)
+                for (int l = 0; l < Integers.Count; l++)
+                for (int o = 0; o < Integers.Count; o++)
+                for (int u = 0; u < Integers.Count; u++)
+                        {
+
+
+                        }
+            Console.ReadLine();
+        }
+    }
+}
