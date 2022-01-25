f25537988c7150df91b9f3e71ae52c6f91821feb
TheDapperDemon <83481628+TheDapperDemon@users.noreply.github.com>
2022-01-25 13:23:38 -0700

    created Pt7

--- ConsoleAppPt7/ConsoleAppPt7/Program.cs
+++ ConsoleAppPt7/ConsoleAppPt7/Program.cs
@@ -3,22 +3,37 @@ using System.Collections.Generic;
 
 namespace ConsoleAppPt7
 {
+    
     class Program
     {
         static void Main(string[] args)
         {
-            List<string> Integers = new List<string>() { "35", "15", "22", "91" };
-            Console.WriteLine("Please enter a number to be divide by each number in the list");
-            var answer = Console.ReadLine();
-                for (int i = 0; i < Integers.Count; i++)
-                for (int l = 0; l < Integers.Count; l++)
-                for (int o = 0; o < Integers.Count; o++)
-                for (int u = 0; u < Integers.Count; u++)
-                        {
 
+            try
+            {
+                List<int> Integers = new List<int>() { 35, 15, 25, 95 };
+                Console.WriteLine("Please enter a number to be divide by each number in the list");
+                int answer = Convert.ToInt32(Console.ReadLine());
+           
+                foreach (int i in Integers)
+                {
+                    Console.WriteLine(i / answer);
+                }
+                Console.ReadLine();
+            }
+           catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
 
-                        }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+            Console.WriteLine("Try Catch Block has been Executed");
             Console.ReadLine();
+            
         }
     }
 }
