832c72941fa7b80577399de573cf1c5b0fe23e8b
TheDapperDemon <83481628+TheDapperDemon@users.noreply.github.com>
2022-01-28 15:09:13 -0700

    Created Calling Methods

---
+++ CallingMethods/CallingMethods/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CallingMethods
+{
+    public class Class1
+    {
+        public static int Method1(int A)
+        {
+          
+            Console.WriteLine("Int A " + 10);
+
+        }
+        static void Method2()
+        {
+
+        }
+        static void Method3()
+        {
+
+        }
+
+    }
+}
---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Method1(20); 
+        }
+    }
+}
