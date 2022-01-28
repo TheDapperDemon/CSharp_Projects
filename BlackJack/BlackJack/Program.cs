71a5bf3617a02c7ad0e9e6f682ee0c64c193cb1c
TheDapperDemon <83481628+TheDapperDemon@users.noreply.github.com>
2022-01-28 14:24:35 -0700

    added Shuffle Command to BlackJack

--- BlackJack/BlackJack/Program.cs
+++ BlackJack/BlackJack/Program.cs
@@ -8,6 +8,7 @@ namespace BlackJack
         static void Main(string[] args)
         {
             Deck deck = new Deck();
+            deck = Shuffle(deck);
 
             foreach (Card card in deck.Cards)
             {
@@ -15,6 +16,22 @@ namespace BlackJack
             }
             Console.WriteLine(deck.Cards.Count);
             Console.ReadLine();
+        }
+
+        public static Deck Shuffle(Deck deck)
+        {
+            List<Card> TempList = new List<Card>();
+            Random random = new Random();
+
+            while (deck.Cards.Count > 0)
+            {
+                int randomIndex = random.Next(0, deck.Cards.Count);
+                TempList.Add(deck.Cards[randomIndex]);
+                deck.Cards.RemoveAt(randomIndex);
+            }
+            deck.Cards = TempList;
+            return deck;
+
         }
     }
 }
