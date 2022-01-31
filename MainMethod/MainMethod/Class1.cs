e08ce64d6d7b66caebf773a1c6e57d20ec8815e7
TheDapperDemon <83481628+TheDapperDemon@users.noreply.github.com>
2022-01-31 14:57:45 -0700

    updated blackjack and created mAINmETHOD

---
+++ MainMethod/MainMethod/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MainMethod
+{
+    public class Class1
+    {
+        public static int Method1(int A)
+        {
+            int C = A + 10;
+            return C;
+        }
+    }
+}
---
+++ MainMethod/MainMethod/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MainMethod
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Class1.Method1(Convert.ToInt32(Console.ReadLine()));
+
+        }
+        
+    }
+}
