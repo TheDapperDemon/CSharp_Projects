2085c70ee121c983d3fe358d91e495f619f275e6
TheDapperDemon <83481628+TheDapperDemon@users.noreply.github.com>
2022-02-04 16:10:40 -0700

    commented out assignment details for personal reference

--- VoidMethod/VoidMethod/Program.cs
+++ VoidMethod/VoidMethod/Program.cs
@@ -1,4 +1,26 @@
 ﻿using System;
+//Create a class. In that class, create a void method that takes two integers as parameters. Have the method do a math operation on the first integer and display the second integer to the screen.
+
+//In the Main() method of the console app, instantiate the class.
+
+//Call the method in the class, passing in two numbers.
+
+//Call the method in the class, specifying the parameters by name.
+
+//PART TWO BELOW 
+
+//Create a class. In that class, create a void method that outputs an integer. Have the method divide the data passed to it by 2.
+
+//In the Main() method, instantiate that class.
+
+//Have the user enter a number. Call the method on that number. Display the output to the screen. It should be the entered number, divided by two.
+
+//Create a method with output parameters.
+
+//Overload a method.
+
+//Declare a class to be static
+
 
 namespace VoidMethod
 {
