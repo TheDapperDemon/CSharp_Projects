f57d94203be6343fa34d02a8cb519a5406cd89be
TheDapperDemon <83481628+TheDapperDemon@users.noreply.github.com>
2022-02-04 14:40:32 -0700

    Created VoidMethod

---
+++ VoidMethod/VoidMethod/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VoidMethod
+{
+    class Class1
+    {
+     
+public int Method1(int answer1, int answer2 = 18)
+{
+    int Operation = answer1 + answer2;
+    return Operation;
+}
+    }
+}
---
+++ VoidMethod/VoidMethod/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace VoidMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 Grape = new Class1();
+
+
+
+
+            Console.WriteLine("Please enter a number to be used in the program");
+            int answer1 = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                Console.WriteLine("Please enter a second number to be used in the program. NOT REQUIRED");
+                int answer2 = Convert.ToInt32(Console.ReadLine());
+                int result = Grape.Method1(answer1, answer2);
+                Console.WriteLine("Your two numbers ran through the program return " + result);
+            }
+            catch
+            {
+                int result = Grape.Method1(answer1);
+                Console.WriteLine("The result of your number is " + result);
+                Console.ReadLine();
+            }
+        }
+    }
+}
