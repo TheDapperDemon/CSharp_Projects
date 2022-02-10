93dd2d68bdd6f369a5c132e3122393108c97b3f1
TheDapperDemon <83481628+TheDapperDemon@users.noreply.github.com>
2022-02-10 15:00:32 -0700

    Created Methods and Objects with Persons nd Emplyee classes

---
+++ Methods and Objects/Methods and Objects/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods_and_Objects
+{
+    public class Employee : Person
+    {
+        List<int> id = new List<int>()
+        {
+            123, 456, 789, 000
+        };
+    }
+}
---
+++ Methods and Objects/Methods and Objects/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods_and_Objects
+{
+    public class Person
+    {
+        new List<string> FirstName;
+        new List<string> LastName;
+    }
+    static void MethodSayName()
+    {
+        Console.WriteLine("Name:[full name]");
+    }
+}
