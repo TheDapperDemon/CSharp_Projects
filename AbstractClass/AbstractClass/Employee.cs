1deae22a1abd411686cda50e476434defb39401b
TheDapperDemon <83481628+TheDapperDemon@users.noreply.github.com>
2022-02-14 15:24:47 -0700

    Created AbstractClass

---
+++ AbstractClass/AbstractClass/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractClass
+{
+    public class Employee : Person
+    {
+        public int id { get; set; }
+        public void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+        }
+    }
+}
---
+++ AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Jonah = new Employee()
+            {
+                FirstName = "Sample",
+                LastName = "Student"
+            };
+            Jonah.SayName();
+            Console.ReadLine();
+        }
+    }
+}
