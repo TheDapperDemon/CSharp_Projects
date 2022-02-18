2edc8c9774aeb4b186f77c375d772812831ba988
TheDapperDemon <83481628+TheDapperDemon@users.noreply.github.com>
2022-02-18 13:04:57 -0700

    finished operators

--- Operators/Operators/Employee.cs
+++ Operators/Operators/Employee.cs
@@ -7,11 +7,12 @@ namespace Operators
     public class Employee : Person
     {
         public int id { get; set; }
-        public static bool Operator(Employee Employee1, Employee Employee2)
+
+        public static bool operator ==(Employee Employee1, Employee Employee2)
         {
             return Employee1.id == Employee2.id;
         }
-        public static bool Operator2(Employee Employee1, Employee Employee2)
+        public static bool operator !=(Employee Employee1, Employee Employee2)
         {
             return Employee1.id != Employee2.id;
         }
