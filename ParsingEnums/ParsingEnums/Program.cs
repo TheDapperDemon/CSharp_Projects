4c0eff7ba5c9a74840f736d6677b7da7a95a18b2
TheDapperDemon <83481628+TheDapperDemon@users.noreply.github.com>
2022-02-21 17:06:22 -0700

    Updated ParsingEnums

---
+++ ParsingEnums/ParsingEnums/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ParsingEnums
+{
+    class Program : Days
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please type the current day of the week");
+            string value = Console.ReadLine();
+            Days Day = (Days)Enum.Parse(typeof(Day), value);
+            //answer = Convert.ToInt32();
+        }
+    }
+}
