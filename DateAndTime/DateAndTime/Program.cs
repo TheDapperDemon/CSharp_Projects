52a1e52c7ebdd139613bbf458b057b2a71253992
TheDapperDemon <83481628+TheDapperDemon@users.noreply.github.com>
2022-03-02 12:59:50 -0700

    Created DateandTime Assignment

---
+++ DateAndTime/DateAndTime/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DateAndTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = new DateTime(2022, 03, 02, 12, 52, 18);
+            Console.ReadLine();
+            Console.WriteLine("Please enter a number");
+            int X = Console.ReadLine();
+        }
+    }
+}
