519de8e240badcd7e6b58384081f78a6538e4eb6
TheDapperDemon <83481628+TheDapperDemon@users.noreply.github.com>
2022-03-02 13:25:06 -0700

    finished DateandTime

--- DateAndTime/DateAndTime/Program.cs
+++ DateAndTime/DateAndTime/Program.cs
@@ -6,10 +6,11 @@ namespace DateAndTime
     {
         static void Main(string[] args)
         {
-            DateTime dateTime = new DateTime(2022, 03, 02, 12, 52, 18);
+            Console.WriteLine("The current Date and Time is " + DateTime.Now);
+            Console.WriteLine("Please enter a number (x) to be added to the Current Time in hours");
+            int X = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The time in x hours from now will be " + DateTime.Now.AddHours(X));
             Console.ReadLine();
-            Console.WriteLine("Please enter a number");
-            int X = Console.ReadLine();
         }
     }
 }
